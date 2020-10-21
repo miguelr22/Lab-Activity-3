17725837fbf7b458fdb5440f1753fd74b761a439
miguelr22 <72761857+miguelr22@users.noreply.github.com>
2020-10-20 19:30:56 -0500

    Add files via upload

---
+++ Miguel Rodriguez CSS 225 Problem 1 Player Actions.py
@@ -0,0 +1,16 @@
+#player actions
+
+#This will make the user input usable when responding at the bottom.
+def check_play_again(user_input):
+    #Place your code here
+#This "If" and "Elif" let you respond with "y" or "n" if you want to play.
+#The else statement will say "incorect" if given a different response.
+    if user_input == "y"
+        print("Play Again!")
+        elif user_input== "n":
+            print("Quit Game!")
+        else:
+            print("Incorrect!:)
+#This input will ask you if you want to play again with the possible responses shown.
+check_play_again(input("Would you like to play again? Type Y for Yes or N for No.
+                       
---
+++ Miguel Rodriguez CSS Problem 2 shop.py
@@ -0,0 +1,20 @@
+#shop.py
+
+#This will allow the total cost and the customer money work.
+def check_money(total_cost, customer_money):
+    #Place code here
+#This will tell you if you do or do not have enough money.
+    if total_cost > customer_money
+    print ("Not Enough Money!")
+    return False
+elif total_cost <= customer_money:
+    print ("Processed Moeny")
+    return True
+#This will be checking if you have enough money.
+#This should print False
+can_pay = check_money(107,49)
+print(can_pay)
+
+#This should print True
+can_pay =check_money(6,88)
+print(can_pay)
